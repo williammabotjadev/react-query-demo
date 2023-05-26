e89a2d30cf0b14d58a08d434c145cd85a78bf65a
williammabotjadev <williammabotjadev@gmail.com>
2023-05-26 15:48:49 +0200

    DotLoader, react-spinners

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import { useQuery } from '@tanstack/react-query';
+import  DotLoader  from 'react-spinners/DotLoader';
 
 function App() {
+
+  const fetchTodoList = () => {
+
+  }
+
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['todos'],
+    queryFn: fetchTodoList,
+  });
+
   return (
     <div className="App">
       
